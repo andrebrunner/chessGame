740fa0d11490a1aa6d4a52366a03c71e184a9439
André Brunner <andre.brunner@wilken.de>
2021-12-16 14:09:00 +0100

    Initial Commit

---
+++ src/app/chessgame/chessgame.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChessgameComponent } from './chessgame.component';
+
+describe('ChessgameComponent', () => {
+  let component: ChessgameComponent;
+  let fixture: ComponentFixture<ChessgameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ChessgameComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChessgameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/chessgame/chessgame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chessgame',
+  templateUrl: './chessgame.component.html',
+  styleUrls: ['./chessgame.component.css']
+})
+export class ChessgameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
